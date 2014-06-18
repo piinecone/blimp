9285a772d117d7663a980e4fc7fc1ff4df851b17
Nicholas Hughes <hughes.nick@gmail.com>
2014-06-18 00:01:41 -0700

    Improve `blimp pull` functionality

--- lib/blimp/pull.rb
+++ lib/blimp/pull.rb
@@ -7,11 +7,10 @@ module Blimp
       # do not overwrite local files unless the -f option is used
       puts "HEAD is at #{current_sha}"
       puts "Downloading assets from #{remote_prefix}"
-      puts s3.bucket.objects.with_prefix("#{remote_prefix}").count
       objects = s3.bucket.objects.with_prefix(remote_prefix)
       objects.each_with_index do |object, index|
         local_key = local_key_for_object(object)
-        puts "(#{index+1}/#{objects.count}) Downloading #{object.key} to #{local_key}..."
+        puts "(#{index+1}/#{objects.count}) Writing #{object.key} to #{local_key}..."
         download object, local_key
       end
     end
@@ -20,11 +19,27 @@ module Blimp
 
     def self.download(object, path)
       if File.exists?(path)
-        puts "Download aborted: #{path} already exists."
-      else
-        File.open(path, "w") do |f|
-          f.write object.read
+        if File.mtime(path) > object.last_modified
+          puts "--------------------------------------------------------------------------------------"
+          puts "Warning: #{path}"
+          puts "Your local version of this file is more recent. Are you sure you want to overwrite it?"
+          puts "--------------------------------------------------------------------------------------"
+          puts "Overwrite #{path}? [y/n]"
+          response = gets.chomp
+          if response == 'y'
+            write_object_to_path object, path
+          else
+            puts "Skipping #{object.key}. Push, delete, or move this file and `blimp pull` to resynchronize."
+          end
         end
+      else
+        write_object_to_path object, path
+      end
+    end
+
+    def self.write_object_to_path(object, path)
+      File.open(path, "w") do |f|
+        f.write object.read
       end
     end
 
@@ -45,7 +60,7 @@ module Blimp
     end
 
     def self.current_sha
-      git.current_sha
+      @current_sha ||= git.current_sha
     end
   end
 end
