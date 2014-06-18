ab700bcab56fc820614c74b57aad1f700dd0337f
Nicholas Hughes <hughes.nick@gmail.com>
2014-06-17 17:16:39 -0700

    Add blimp status function

--- lib/blimp/status.rb
+++ lib/blimp/status.rb
@@ -1,8 +1,14 @@
 module Blimp
   module Status
     def self.run!
-      puts 'Running blimp status...'
-      puts 'Done!'
+      system('git status -b -s --ignored')
+      puts "Determining last SHA for storage location..."
+      value = `git rev-parse --verify HEAD`
+      if $?.exitstatus == 0
+        puts "Assets for #{value} will be stored at internet.com:/#{value}"
+      else
+        puts "Could not determine a SHA. Please commit something with git!"
+      end
     end
   end
 end
