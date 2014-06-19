1ddcb02b13dec5c328e44a5647ddce6b7dc346c1
Nicholas Hughes <hughes.nick@gmail.com>
2014-06-19 16:42:46 -0700

    Prefer File.open to File.read when generating checksum on Windows

--- lib/blimp/utils.rb
+++ lib/blimp/utils.rb
@@ -3,8 +3,9 @@ require 'digest'
 module Blimp
   module Utils
     def self.file_and_object_match?(filepath, object)
-      # object.exists? && object.etag.gsub(/"/, '') == Digest::MD5.hexdigest(File.read(filepath))
-      object.exists? && object.content_length == File.stat(filepath).size
+      # TODO fix this so it works identically on OS X and Windows
+      object.exists? && object.etag.gsub(/"/, '') == Digest::MD5.hexdigest(File.open(filepath, 'rb') {|f| f.read})
+      # object.exists? && object.content_length == File.stat(filepath).size
     end
   end
 end
