4c7dd8c63dba1b162c74bab4438b6a315d63a24e
Nicholas Hughes <hughes.nick@gmail.com>
2014-06-19 15:33:10 -0700

    Compare remote and local objects by file size

--- lib/blimp/utils.rb
+++ lib/blimp/utils.rb
@@ -3,7 +3,8 @@ require 'digest'
 module Blimp
   module Utils
     def self.file_and_object_match?(filepath, object)
-      object.exists? && object.etag.gsub(/"/, '') == Digest::MD5.hexdigest(File.read(filepath))
+      # object.exists? && object.etag.gsub(/"/, '') == Digest::MD5.hexdigest(File.read(filepath))
+      object.exists? && object.content_length == File.stat(filepath).size
     end
   end
 end
